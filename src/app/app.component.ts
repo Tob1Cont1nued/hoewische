2e26365880b3fc454552892278eb4e89ca828382
Tobias_Konken <tobias.konken@beyonnex.io>
2024-10-30 16:43:17 +0100

    colors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +10,34 @@ export class AppComponent {
   // Die Methode scrollTo wird verwendet, um zum gewünschten Abschnitt zu scrollen
   scrollTo(section: string) {
     document.getElementById(section)?.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  isNavbarVisible = true; // Initial sichtbar
+  lastScrollTop = 0; // Letzte Scroll-Position
+  minWidth = 450;
+  minHeight = 950;
+  
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    // Überprüfe die aktuelle Bildschirmgröße
+    const currentWidth = window.innerWidth;
+    const currentHeight = window.innerHeight;
+
+    // Wenn die Bildschirmgröße kleiner ist als die festgelegten Werte, gehe mit der Logik fort
+    if (currentWidth < this.minWidth && currentHeight < this.minHeight) {
+      const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+      // Überprüfe die Scrollrichtung
+      if (currentScrollTop > this.lastScrollTop) {
+        // Wenn nach unten gescrollt wird
+        this.isNavbarVisible = false; // Navbar ausblenden
+      } else {
+        // Wenn nach oben gescrollt wird
+        this.isNavbarVisible = true; // Navbar einblenden
+      }
+
+      // Aktualisiere die letzte Scroll-Position
+      this.lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop; // Verhindert negative Scroll-Positionen
+    }
   }
 }
