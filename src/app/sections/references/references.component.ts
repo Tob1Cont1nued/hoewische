d762d08d0ed4860d958e57ad4b867962d3b54217
Tobias_Konken <tobias.konken@beyonnex.io>
2024-11-12 09:36:08 +0100

    paginator fix

--- src/app/sections/references/references.component.ts
+++ src/app/sections/references/references.component.ts
@@ -1,6 +1,8 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
 import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
+import { isPlatformBrowser } from '@angular/common';
+
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
@@ -12,33 +14,39 @@ Swiper.use([Navigation]);
   templateUrl: './references.component.html',
   styleUrl: './references.component.css'
 })
+
 export class ReferencesComponent implements AfterViewInit {
+  
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
   ngAfterViewInit(): void {
-    // Initialisiere Swiper mit Navigation und Pagination
-    const swiper = new Swiper('.swiper-container', {
-      modules: [Navigation, Pagination],
-      slidesPerView: 1,
-      spaceBetween: 20,
-      loop: true,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      breakpoints: {
-        768: {
-          slidesPerView: 2,
-          spaceBetween: 30
+    // Prüfe, ob der Code im Browser läuft
+    if (isPlatformBrowser(this.platformId)) {
+      // Initialisiere Swiper nur, wenn wir im Browser sind
+      const swiper = new Swiper('.swiper-container', {
+        modules: [Navigation, Pagination],
+        slidesPerView: 1,
+        spaceBetween: 20,
+        loop: true,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
         },
-        1024: {
-          slidesPerView: 3,
-          spaceBetween: 40
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+        breakpoints: {
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 30
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 40
+          }
         }
-      }
-    });
+      });
+    }
   }
 }
