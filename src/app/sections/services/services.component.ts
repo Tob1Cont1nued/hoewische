d40105aa59586ab829cc7a7cbbc6969a5ed843f9
Tobias_Konken <tobias.konken@beyonnex.io>
2024-11-25 13:44:39 +0100

    services

--- src/app/sections/services/services.component.ts
+++ src/app/sections/services/services.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrl: './services.component.css'
 })
 export class ServicesComponent {
-
+  services = [
+    { 
+      title: 'Kategorie 1', 
+      description: 'Kurzbeschreibung für Kategorie 1.', 
+      image: '/assets/images/kategorie1.png' 
+    },
+    { 
+      title: 'Kategorie 2', 
+      description: 'Kurzbeschreibung für Kategorie 2.', 
+      image: '/assets/images/kategorie2.png' 
+    },
+    { 
+      title: 'Kategorie 3', 
+      description: 'Kurzbeschreibung für Kategorie 3.', 
+      image: '/assets/images/kategorie3.png' 
+    },
+    { 
+      title: 'Kategorie 4', 
+      description: 'Kurzbeschreibung für Kategorie 4.', 
+      image: '/assets/images/kategorie4.png' 
+    },
+    { 
+      title: 'Kategorie 5', 
+      description: 'Kurzbeschreibung für Kategorie 5.', 
+      image: '/assets/images/kategorie5.png' 
+    },
+    { 
+      title: 'Kategorie 6', 
+      description: 'Kurzbeschreibung für Kategorie 6.', 
+      image: '/assets/images/kategorie6.png' 
+    }
+  ];
 }
