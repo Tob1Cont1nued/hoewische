8745100cac1029e616598061780eed2a04ac3ccd
Tobias_Konken <tobias.konken@beyonnex.io>
2024-11-25 11:51:16 +0100

    anpassungen css

--- src/app/sections/about/about.component.ts
+++ src/app/sections/about/about.component.ts
@@ -6,14 +6,11 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+
   isContentHidden = false; // Zustand für die Sichtbarkeit des Inhalts
-  buttonLabel = 'Inhalt ausblenden'; // Beschriftung des Buttons
 
   toggleContent(): void {
     // Zustand umschalten
     this.isContentHidden = !this.isContentHidden;
-
-    // Button-Beschriftung entsprechend aktualisieren
-    this.buttonLabel = this.isContentHidden ? 'Inhalt anzeigen' : 'Inhalt ausblenden';
   }  
 }
