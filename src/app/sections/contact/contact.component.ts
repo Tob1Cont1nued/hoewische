75f50c053c05a29ea21b6e45e786a48263897406
Tobias_Konken <tobias.konken@beyonnex.io>
2025-01-07 14:18:15 +0100

    nav button

--- src/app/sections/contact/contact.component.ts
+++ src/app/sections/contact/contact.component.ts
@@ -6,6 +6,11 @@ import { Component } from '@angular/core';
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+ 
+  startNavigation(): void {
+    window.open(' https://maps.app.goo.gl/wZ4fQLKUmWSqhVF68', '_blank');
+  }
+
   bookAppointment(): void {
     window.open('https://docs.google.com/forms/d/1Jerd2YHUmmXGED1rc67Cc2kiErptUv5exB4GOCBRG4I/edit', '_blank');
   }
